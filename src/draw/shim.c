7dd87c6681a33de9f0ca90bffeb90cbe2e3d940e
ImOverlord <=>
2018-03-28 17:43:20 +0200

    Improved Shim drawing function to prevent memory related problems

--- src/draw/shim.c
+++ src/draw/shim.c
@@ -7,14 +7,21 @@
 
 #include "my_rpg.h"
 
+/*
+** Needs to be improved to work with all window sizes
+*/
+
 static int color_shim(sfUint8 *pixels, sfColor color)
 {
+	if (pixels == NULL)
+		return (1);
 	for (int i = 0; i < 800*600*4; i+= 4) {
 		pixels[i] = color.r;
 		pixels[i + 1] = color.g;
 		pixels[i + 2] = color.b;
 		pixels[i + 3] = 100;
 	}
+	return (0);
 }
 void draw_shim(game_global_t *game, sfColor color)
 {
@@ -24,9 +31,12 @@ void draw_shim(game_global_t *game, sfColor color)
 
 	if (pixels == NULL)
 		engine_exit(game);
-	color_shim(pixels, color);
+	if (color_shim(pixels, color))
+		engine_exit(game);
 	sfTexture_updateFromPixels(shim_texture, pixels, 800, 600, 0, 0);
 	sfSprite_setTexture(shim_sprite, shim_texture, sfTrue);
 	sfRenderWindow_drawSprite(game->window, shim_sprite, NULL);
 	free(pixels);
+	sfSprite_destroy(shim_sprite);
+	sfTexture_destroy(shim_texture);
 }
