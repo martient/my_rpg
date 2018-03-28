bf35634b6f3f0a0d468e884376977522b5d95266
ImOverlord <=>
2018-03-28 17:44:29 +0200

    Added basic function to draw bars, can be used for loading or health, still being improved

---
+++ src/draw/bar.c
@@ -0,0 +1,52 @@
+/*
+** EPITECH PROJECT, 2018
+** bar.c
+** File description:
+** function to draw info bar
+*/
+
+#include "my_rpg.h"
+
+static int color_bar(sfUint8 *pixels, sfColor color, int width, int height)
+{
+	if (pixels == NULL)
+		return (1);
+	for (int i = 0; i < width*height*4; i+= 4) {
+		pixels[i] = color.r;
+		pixels[i + 1] = color.g;
+		pixels[i + 2] = color.b;
+		pixels[i + 3] = color.a;
+	}
+	return (0);
+}
+
+static int get_bar_size(int max, int percentage)
+{
+	int val;
+
+	val = (percentage * max) / 100;
+	return (val);
+}
+
+void draw_bar(game_global_t *game, sfColor colors[2], int *infos)
+{
+	int front_width = get_bar_size(infos[0], infos[2]);
+	sfUint8 *pixels_back = malloc(infos[0] * infos[1] * 4);
+	sfUint8 *pixels_front = malloc(front_width * infos[1] * 4);
+	sfTexture *bar_texture = sfTexture_create(infos[0], infos[1]);
+	sfSprite *bar_sprite = sfSprite_create();
+	sfVector2f pos = {infos[3], infos[4]};
+
+	if (pixels_front == NULL || pixels_back == NULL)
+		engine_exit(game);
+	color_bar(pixels_back, colors[0], infos[0], infos[1]);
+	color_bar(pixels_front, colors[1], front_width, infos[1]);
+	sfTexture_updateFromPixels(bar_texture, pixels_back, infos[0], infos[1], 0, 0);
+	sfTexture_updateFromPixels(bar_texture, pixels_front, front_width, infos[1], 0, 0);
+	sfSprite_setTexture(bar_sprite, bar_texture, sfTrue);
+	sfSprite_setPosition(bar_sprite, pos);
+	sfRenderWindow_drawSprite(game->window, bar_sprite, NULL);
+	free(pixels_front);
+	sfSprite_destroy(bar_sprite);
+	sfTexture_destroy(bar_texture);
+}
