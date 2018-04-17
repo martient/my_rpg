373d8f0f42c7cc9ddcfb6e357e14ad0aed795754
ImOverlord <=>
2018-04-17 17:49:25 +0200

    Added language pack for multiple keyboard layout

---
+++ language/fr_fr/keyboard.h
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2018
+** keyboard.h
+** File description:
+** keyboard layout fr_fr
+*/
+
+#ifndef KEYBOARD_H_
+	#define KEYBOARD_H_
+
+#include <SFML/Graphics.h>
+
+#define UP_KEY		(sfKeyZ)
+#define DOWN_KEY	(sfKeyS)
+#define LEFT_KEY	(sfKeyQ)
+#define RIGHT_KEY	(sfKeyD)
+
+
+#endif /* !KEYBOARD_H_ */
---
+++ language/us_en/keyboard.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2018
+** keyboard.h
+** File description:
+** keyboard layout us_en
+*/
+
+#ifndef KEYBOARD_H_
+	#define KEYBOARD_H_
+
+#include <SFML/Graphics.h>
+
+#define UP_KEY		(sfKeyW)
+#define DOWN_KEY	(sfKeyS)
+#define LEFT_KEY	(sfKeyA)
+#define RIGHT_KEY	(sfKeyD)
+
+#endif /* !KEYBOARD_H_ */
