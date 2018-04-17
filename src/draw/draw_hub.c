1b964254808b06cf334cfc3272cedcb7df643d40
ImOverlord <=>
2018-04-17 15:29:55 +0200

    Fixed camera, where map would apear to move

--- src/draw/draw_hub.c
+++ src/draw/draw_hub.c
@@ -7,12 +7,23 @@
 
 #include "my_rpg.h"
 
+static void player_cam(game_global_t *game)
+{
+	sfVector2f pos = {game->player->x + (39/2), game->player->y + (39/2)};
+	sfFloatRect cam = {0, 0, game->width, game->height};
+
+	game->player->camera =  sfView_createFromRect(cam);
+	sfView_setCenter(game->player->camera, pos);
+	sfRenderWindow_setView(game->window, game->player->camera);
+}
+
 void draw__(game_global_t *game)
 {
 	sfRenderWindow_clear(game->window, sfWhite);
 	draw_element(game);
 	draw_ui(game);
 	draw_spell_cd_box(game, 100);
+	player_cam(game);
 	// draw_player(game);
 	sfRenderWindow_display(game->window);	
 }
--- src/draw/draw_player.c
+++ src/draw/draw_player.c
@@ -7,15 +7,6 @@
 
 #include "my_rpg.h"
 
-static void player_cam(game_global_t *game)
-{
-	sfVector2f pos = {game->player->x + (39/2), game->player->y + (39/2)};
-	sfFloatRect cam = {0, 0, game->width, game->height};
-
-	game->player->camera =  sfView_createFromRect(cam);
-	sfView_setCenter(game->player->camera, pos);
-	sfRenderWindow_setView(game->window, game->player->camera);
-}
 
 void draw_player(game_global_t *game)
 {
@@ -34,5 +25,5 @@ void draw_player(game_global_t *game)
 	if (anim > 2)
 		anim = 0;
 	game->player->moving = 0;
-	player_cam(game);
+	// player_cam(game);
 }
