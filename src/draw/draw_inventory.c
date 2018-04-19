67728853b6939459192afdc43209cfdfd55faffd
ImOverlord <=>
2018-04-19 18:08:46 +0200

    Added inventory box drawing, when key I pressed

--- src/draw/draw_inventory.c
+++ src/draw/draw_inventory.c
@@ -12,6 +12,8 @@ int draw_inventory(game_global_t *game)
 	player_info_t *player = game->player;
 	sfColor sfBrown = {182, 155, 76, 255};
 
+	if (game->inventory_show == 0)
+		return (1);
 	int info[5] = {300, 350, 100, player->x + 100, player->y - (game->height / 2) + 150};
 	sfColor colors[2] = {sfRed, sfBrown};
 	draw_bar(game, colors, info);
