9ee09b00728b14619c9d8193e6a2414a28469bc3
Martient <martient.pro@gmail.com>
2018-04-26 09:23:18 +0200

    Add new function for read items.json and inject new elements in link list

---
+++ src/json_parser/rpg_items_json.c
@@ -0,0 +1,72 @@
+/*
+** EPITECH PROJECT, 2018
+** rpg_spawn_json.c
+** File description:
+** Epitech project
+*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "my_rpg.h"
+
+static int rpg_json_items_data_list(items_data_first_t *first, const nx_json *items)
+{
+	int *data = malloc(sizeof(int) * 3);
+
+	if (!data) {
+		my_putstr("Error: malloc not found\n");
+		return (-1);
+	}
+	data[0] = nx_json_get(items, "id")->int_value;
+	data[1] = nx_json_get(items, "type")->int_value;
+	rpg_spawn_inject(nx_json_get(items, "name")->text_value, data,
+	nx_json_get(items, "texture")->text_value,
+	nx_json_get(items, "stats")->dbl_value);
+	free(data);
+	return (0);
+}
+
+static char *rpg_items_data_load(char *filepath)
+{
+	char tmp_b[2];
+	char *board = NULL;
+	int lenght = 0;
+	int fd = 0;
+
+	fd = open(filepath, O_RDONLY);
+	if (fd < 0) {
+		my_putstr("Error: mob_spawn.json not found\n");
+		return (NULL);
+	}
+	while (read(fd, tmp_b, 1))
+		lenght += 1;
+	close(fd);
+	board = malloc(sizeof(char) * lenght + 1);
+	fd = open(filepath, O_RDONLY);
+	read(fd, board, lenght);
+	board[lenght] = '\0';
+	close(fd);
+	return board;
+}
+
+int rpg_json_init_items_data(items_data_first_t *first)
+{
+	char *read_file = rpg_spawn_load("./resources/items.json");
+	const nx_json* json;
+	const nx_json* items;
+	const nx_json* tmp;
+	if (!read_file) {
+		my_putstr("Error: File not found\n");
+		return (-1);
+	}
+	json = nx_json_parse(read_file, 0);
+	if (json) {
+		items = nx_json_get(json, "items");
+		for (int i = 0; i < items->length; i++) {
+			tmp = nx_json_item(items, i);
+			rpg_json_spawn_list(first, tmp);
+		}
+	}
+	return (0);
+}
