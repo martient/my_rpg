a833ffd4d504174a603628f346502b3fbd7bda19
ImOverlord <=>
2018-05-12 14:46:00 +0200

    Fixed SigSev when discarding empty items

--- src/quest/quest_manager.c
+++ src/quest/quest_manager.c
@@ -20,10 +20,8 @@ void quest_manager(game_global_t *game)
 	player_info_t *player = game->player;
 	quests_t quest_list[3] = {no_quest, is_harvest_done, no_quest};
 
-	printf("Quest %d\n", player->current_quest_id);
 	if (player->current_quest_id >= 3) {
 		if (quest_list[player->current_quest_id - 2](game) == 0) {
-			printf("RESET\n");
 			player->current_quest_id = 0;
 		}
 	}
